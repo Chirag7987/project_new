b502ccfdf7f0e4978334e963196b6f7ff1ead0e4
chirag <chiragthakur8006@gmail.com>
2025-02-01 19:38:04 +0530

    favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { twMerge } from "tailwind-merge";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Core-Intels",
+  title: "Coreintels",
   description: "Template created by Frontend Tribe",
 };
 
